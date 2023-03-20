0fa45b652842b762e931d4516b575b924daaa9cf
Arm_PC <kantawitsakhet@gmail.com>
2023-03-20 18:41:26 +0700

    lab 5

---
+++ lab4/lexer.py
@@ -0,0 +1,158 @@
+from typing import NamedTuple
+from enum import Enum
+
+
+# -----------------------------------------------------------------------------
+ADD_OPERATOR = "+-"
+MULTIPLY_OPERATOR = "*/%"
+POWER_OPERATOR = "^"
+ALL_OPERATOR = ADD_OPERATOR + MULTIPLY_OPERATOR + POWER_OPERATOR
+
+
+# -----------------------------------------------------------------------------
+class TokenType(Enum):
+    """The token type."""
+    UNKNOWN = 0
+    END_OF_TEXT = 1
+    NUMBER = 2
+    IDENTIFIER = 3
+    ADD_OP = 4
+    MUL_OP = 5
+    POWER_OP = 6
+    LEFT_PAREN = 7
+    RIGHT_PAREN = 8
+    
+
+# -----------------------------------------------------------------------------
+class Token(NamedTuple):
+    """The token class."""
+    token_type: TokenType = TokenType.UNKNOWN
+    lexeme: str = ""
+
+
+# -----------------------------------------------------------------------------
+def skip_whitespace(text: str, pos: int) -> int:
+    """Skip white spaces in text starting from pos.
+    
+    Args
+    ----
+        text:   the text to be scanned.
+        pos:    the starting position to scan.
+      
+    Returns
+    -------
+        the position of the first non-whitespace starting from pos.
+        
+    """
+    while pos < len(text) and text[pos].isspace():
+        pos = pos + 1
+    
+    return pos
+    
+    
+# -----------------------------------------------------------------------------
+def get_number(text: str, pos: int = 0) -> tuple:
+    """Get the number from text string starting at pos.
+    
+    Args
+    ----
+        text:       the text to be scanned.
+        pos:        the starting position to scan.
+        
+    Returns
+    -------
+        number and the position after the token
+    
+    """
+    cur_pos = pos
+    while cur_pos < len(text) and text[cur_pos].isdigit():
+        cur_pos = cur_pos + 1
+    return text[pos:cur_pos], cur_pos
+        
+    
+# -----------------------------------------------------------------------------
+def get_identifier(text: str, pos: int = 0) -> tuple:
+    """Get the identifier from text string starting at pos.
+    
+    Args
+    ----
+        text:       the text to be scanned.
+        pos:        the starting position to scan.
+        
+    Returns
+    -------
+        identifier and the position after the token
+    
+    """
+    cur_pos = pos
+    while cur_pos < len(text) and text[cur_pos].isalnum():
+        cur_pos = cur_pos + 1
+    return text[pos:cur_pos], cur_pos
+        
+    
+
+# -----------------------------------------------------------------------------
+def get_symbol(text: str, pos: int = 0) -> tuple:
+    """Get the symbol from text string starting at pos.
+    
+    Args
+    ----
+        text:       the text to be scanned.
+        pos:        the starting position to scan.
+        
+    Returns
+    -------
+        symbol and the position after the token
+    
+    """
+    cur_pos = pos
+    while cur_pos < len(text) and text[cur_pos] in ALL_OPERATOR:
+        cur_pos = cur_pos + 1
+    return text[pos:cur_pos], cur_pos
+        
+    
+
+# -----------------------------------------------------------------------------
+def get_token(text: str, pos: int = 0) -> tuple:
+    """Get the token from text string starting at pos.
+    
+    Args
+    ----
+        text:       the text to be scanned.
+        pos:        the starting position to scan.
+        
+    Returns
+    -------
+        token and the position after the token
+    
+    """
+    pos = skip_whitespace(text, pos)
+    token = Token(TokenType.END_OF_TEXT, "")
+    
+    if pos < len(text):
+        if text[pos].isdigit():
+            lexeme, pos = get_number(text, pos)
+            token = Token(TokenType.NUMBER, lexeme)
+        elif text[pos].isalpha():
+            lexeme, pos = get_identifier(text, pos)
+            token = Token(TokenType.IDENTIFIER, lexeme)
+        elif text[pos] in ADD_OPERATOR:
+            if pos+1 < len(text) and text[pos + 1] == text[pos]:
+                lexeme = text[pos:pos+2]
+                token = Token(TokenType.INCREMENT, lexeme)
+            else:
+                lexeme, pos = get_symbol(text, pos)
+                token = Token(TokenType.ADD_OP, lexeme)
+        elif text[pos] in MULTIPLY_OPERATOR:
+            lexeme, pos = get_symbol(text, pos)
+            token = Token(TokenType.MUL_OP, lexeme)
+        elif text[pos] == '(':
+            lexeme, pos = get_symbol(text, pos)
+            token = Token(TokenType.LEFT_PAREN, lexeme)
+        elif text[pos] == ')':
+            lexeme, pos = get_symbol(text, pos)
+            token = Token(TokenType.RIGHT_PAREN, lexeme)
+        else:
+            raise ValueError(f"Unrecognized symbol [{text[pos]}]")
+        
+    return token, pos
--- lab5/main.py
+++ lab5/main.py
@@ -5,18 +5,51 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 
 out_path =  os.path.join( dir_path,'output')
 
+from typing import NamedTuple
+
+class Bytecode(NamedTuple):
+    """
+    The Bytecode class.
+    """
+    function: int = 0
+    level: int = 0
+    value: int = 0
+
 def get_dataFromMCT():
+    data_lines = []
     with open(os.path.join( dir_path,'input.mct'), 'r') as f:
         # print(f.read())
-        data = f.read()
-    return data
+        # data = f.read()
+        for line in f.readlines():
+            # print("line")
+            # print(line)
+            _data = line.replace('\n', ' ').split(" ")
+            # print(_data)
+            # data_line = {}
+            
+            byteCode = Bytecode()
+
+            try:
+                byteCode = Bytecode(int(_data[0]), int(_data[1]), int(_data[2]))
+                # data_line["function"] = int(_data[0])
+                # data_line["level"] = int(_data[1])
+                # data_line["value"] = int(_data[2])
+            except:
+                raise
+            
+            data_lines.append(byteCode)
+                
+    return data_lines
 
 def gen_MC(values: list):
     with open(os.path.join( out_path,'out.mc'), 'w') as f:
+        value: Bytecode
         for value in values:
-            f.write("{0:03b}".format(value[0]))
-            f.write("{0:02b}".format(value[1]))
-            f.write("{0:011b}".format(value[2])+ "\n")
+            # print(value)
+            f.write("{0:03b}".format(value.function))
+            f.write("{0:02b}".format(value.level))
+            f.write("{0:011b}".format(value.value))
+            # f.write("\n")
 
 
 def MC_2_MCT():
@@ -24,56 +57,35 @@ def MC_2_MCT():
         # print(f.read())
         # data = f.read()
         results= []
-        lineNUM = 0
-        for line in f.readlines():
-            lineNUM+=1
-            # print(line)
+        # lineNUM = 0
 
-            validateMC(line, lineNUM)
+        # read 16 character and 
+        MAX_IN_LINE = 16
+        line = ""
+        for char in f.read():
+            line += char
 
-            value = []
-            value.append(int(line[0:3], 2))
-            value.append(int(line[3:5], 2))
-            value.append(int(line[5:16], 2))
+            if(len(line) >= MAX_IN_LINE):
+                byteCode = Bytecode(int(line[0:3], 2), int(line[3:5], 2), int(line[5:16], 2))
+                line = ""
+                results.append(byteCode)
+        
+        # print(results)
 
-            results.append(value)
 
-
-        print(results)
+        # print(results)
     return results
             
+def gen_MCT(values: list):
+    with open(os.path.join( out_path,'out.mct'), 'w') as f:
+        value: Bytecode
+        for value in values:
+            # print(value)
+            f.write("{0} ".format(value.function))
+            f.write("{0} ".format(value.level))
+            f.write("{0} ".format(value.value))
+            f.write("\n")
 
-
-def validateMC(input: list, lineNUM):
-    #TODO: ERROR CHECK LEVEL OF OPERATION IS SUPPORT OR NOT IF SELECT FUNCTION 0, 1, 5, 6, 7
-    #TODO: ERROR CHECK OPERATION IF SELECT FUNCTION 7
-    errMessage=""
-    functionNumber = int(input[0])
-    levelNumber = int(input[1])
-    valueNumber = int(input[0])
-
-    try:
-        # validate data range
-        if(functionNumber > 3 ):
-            errMessage = f"Invalid function [{input[0]}]"
-            
-        if(levelNumber > 2 ):
-            errMessage = f"Invalid level [{input[1]}]"
-        if(valueNumber > 11 ):
-            errMessage = f"Invalid value [{input[2]}]"
-
-        # validateCondition
-        if(str(functionNumber) in "01567" ):
-            if(levelNumber != 0):
-                errMessage = f"The function does not support level [{levelNumber}]"
-        elif (str(functionNumber) in "7"):
-            if(valueNumber > 7 ):
-                errMessage = f"Invalid operation [{input[2]}]"
-
-        if(errMessage != ""):
-            raise ValueError(f"Line {lineNUM}: {errMessage}")
-    except:
-         raise ValueError(f"Line {lineNUM}:  ERROR ")
 
 def disassembler(inputs: list):
     functionDisassembler = {
@@ -86,28 +98,82 @@ def disassembler(inputs: list):
         6: "JPC",
         7: "OPR",      
     }
-    
-    results = []
-    for input in inputs:
-        value = []
-        value.append(functionDisassembler[input[0]])
-        value.append(input[1])
-        value.append(input[2])
-        results.append(value)
 
-    print(results)
-    return results
+    with open(os.path.join( out_path,'out.asm'), 'w') as f:
+        value: Bytecode
+        for value in inputs:
+            # print(value)
+            f.write("{0} ".format(functionDisassembler[value.function]))
+            f.write("{0} ".format(value.level))
+            f.write("{0} ".format(value.value))
+            f.write("\n")
+
+
+def validateMCT_PerLine(input: Bytecode, lineNUM, isHeaderLine: bool=True):
+    #TODO: ERROR CHECK LEVEL OF OPERATION IS SUPPORT OR NOT IF SELECT FUNCTION 0, 1, 5, 6, 7
+    #TODO: ERROR CHECK OPERATION IF SELECT FUNCTION 7
     
+    err = False
+
+    functionNumber = input.function
+    levelNumber = input.level
+    valueNumber = input.value
+
+    header = f"Line {lineNUM}"
+    if(isHeaderLine == False):
+        header = f"Bytes { (lineNUM - 1)*2 } - {(lineNUM - 1)*2 + 1}"
+
+
+    try:
+        # validate data range
+        if(functionNumber > 7 ):
+            err = True
+            print(f"{header}: Invalid function [{functionNumber}]")
+        if(levelNumber > 3 ):
+            err = True
+            print(f"{header}: Invalid level [{levelNumber}]")
+        if(valueNumber > 2047 ):
+            err = True
+            print(f"{header}: Invalid value [{valueNumber}]")
+
+        # validateCondition
+        if(str(functionNumber) in "0 1 5 6 7" ):
+            if(levelNumber != 0):
+                err = True
+                print(f"{header}: The function does not support level [{levelNumber}]")
+        if (str(functionNumber) in "7"):
+            if(valueNumber > 11 ):
+                err = True
+                print(f"{header}: Invalid operation [{valueNumber}]")
+
+        if(err == True):
+            # print(f"Line {lineNUM}: {errMessage}")
+            raise ValueError(f".mct FILE ERROR.")
+    except Exception as error:
+        #  raise ValueError(f"{error.message}")
+        # print('Caught this error: ' + repr(error))
+        raise
 
 def main():
-    data = get_dataFromMCT()
-    gen_MC([[1,2,200], [1,2,200], [1,2,2005] ])
+    # # TODO: PHASE 1, .mct -> .mc
+    data_MCT = get_dataFromMCT()
+    # # [{'function': '0', 'level': '0', 'value': '0'}, {'function': '7', 'level': '0', 'value': '1'}, {'function': '2', 'level': '1', 'value': '2047'}, {'function': '3', 'level': '2', 'value': '79'}, {'function': '5', 'level': '0', 'value': '123'}]
+    gen_MC(data_MCT)
+
+    # # TODO: PHASE 4, validate .mct header error as Line
+    for index, item in enumerate(data_MCT):
+        validateMCT_PerLine(item, index+1, True)
+
+    # TODO: PHASE 5, validate .mct header error as byte
+    for index, item in enumerate(data_MCT):
+        validateMCT_PerLine(item, index+1, False)
+
+    # TODO: PHASE 2, .mc -> .mct
     inputs = MC_2_MCT()
+    gen_MCT(inputs)
+
+    # TODO: PHASE 3, .mc -> .asm
     disassembler(inputs)
-
-
-    # convert to binary
-
 
 if(__name__ == "__main__"):
     main()
